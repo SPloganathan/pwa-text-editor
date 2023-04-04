442d5e7277aec2d8808af6a6a6c58e879f1fd0a6
sploganathan <sakthiloganathan1127@gmail.com>
2023-04-03 17:06:15 -0700

    feat(database): added function for getting and updating indexedDB

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,21 +1,54 @@
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB("pwa_text_editor", 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains("pwa_text_editor")) {
+        console.log("pwa_text_editor database already exists");
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore("pwa_text_editor", {
+        keyPath: "id",
+        autoIncrement: true,
+      });
+      console.log("pwa_text_editor database created");
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  // Create a connection to the database database and version we want to use.
+  const pwaTextEditorDb = await openDB("pwa_text_editor", 1);
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+  // Create a new transaction and specify the database and data privileges.
+  const tx = pwaTextEditorDb.transaction("pwa_text_editor", "readwrite");
 
+  // Open up the desired object store.
+  const store = tx.objectStore("pwa_text_editor");
+
+  // Use the .add() method on the store and pass in the content.
+  const request = store.put({ id: 1, text: content });
+
+  // Get confirmation of the request.
+  const result = await request;
+  console.log("🚀 - data saved to the database", result);
+};
+
+export const getDb = async () => {
+  // Create a connection to the database database and version we want to use.
+  const pwaTextEditorDb = await openDB("pwa_text_editor", 1);
+
+  // Create a new transaction and specify the database and data privileges.
+  const tx = pwaTextEditorDb.transaction("pwa_text_editor", "readonly");
+
+  // Open up the desired object store.
+  const store = tx.objectStore("pwa_text_editor");
+
+  // Use the .get() method to get data in the database.
+  const request = store.get(1);
+
+  // Get confirmation of the request.
+  const result = await request;
+  console.log("result.value", result.text);
+  return result.text;
+};
 initdb();
